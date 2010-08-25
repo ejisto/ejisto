41187318967e228aff065c57e1fa344f14332659
celestino.bellone <celestino.bellone@a1e31c00-292e-b01b-3f4f-04888c7956ed>
2010-08-25 22:52:32 +0000

    import test and general pom
    git-svn-id: https://ejisto.googlecode.com/svn/trunk@7 a1e31c00-292e-b01b-3f4f-04888c7956ed

---
+++ ejisto/test/src/main/java/com/ejisto/hello/beans/HelloWorldBean.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2010 Celestino Bellone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.ejisto.hello.beans;
+
+import java.io.Serializable;
+
+public class HelloWorldBean implements Serializable {
+
+	private static final long serialVersionUID = 6175502203990776213L;
+	private String title = "";
+	private String description = "";
+	private int hits = -1;
+	private long timestamp = 0L;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getHits() {
+		return hits;
+	}
+
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}
---
+++ ejisto/test/src/main/java/com/ejisto/hello/servlets/HelloServlet.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2010 Celestino Bellone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.ejisto.hello.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ejisto.hello.beans.HelloWorldBean;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		HelloWorldBean bean = new HelloWorldBean();
+		PrintWriter pw = response.getWriter();
+		
+		pw.write("<html><head><title>Ejisto Test servlet</title></head><body><b>Title: </b>");
+		pw.write(bean.getTitle());
+		pw.write("<br><b>Description: </b>");
+		pw.write(bean.getDescription());
+		pw.write("<br><b>Hit count: </b>");
+		pw.write(String.valueOf(bean.getHits()));
+		pw.write("<br><b>Time stamp: </b>");
+		pw.write(String.valueOf(bean.getTimestamp()));
+		pw.write("</body><html>");
+		pw.flush();
+		
+	}
+
+}
