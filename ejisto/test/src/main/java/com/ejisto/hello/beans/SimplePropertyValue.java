f6ba105f897b040c171e8c6749dd07fa3976d498
celestino.bellone <celestino.bellone@a1e31c00-292e-b01b-3f4f-04888c7956ed>
2010-12-28 22:59:27 +0000

    first implementation of complex value editor updated dependencies
    git-svn-id: https://ejisto.googlecode.com/svn/trunk@45 a1e31c00-292e-b01b-3f4f-04888c7956ed

---
+++ ejisto/test/src/main/java/com/ejisto/hello/beans/SimplePropertyValue.java
@@ -0,0 +1,43 @@
+/*
+ * Ejisto, a powerful developer assistant
+ *
+ * Copyright (C) 2010  Celestino Bellone
+ *
+ * Ejisto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ejisto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ejisto.hello.beans;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: celestino
+ * Date: 12/20/10
+ * Time: 07:45 PM
+ */
+public class SimplePropertyValue {
+    private String value;
+
+    public SimplePropertyValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "SimplePropertyValue [" + hashCode() + "] = " + getValue();
+    }
+}
