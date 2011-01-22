4f27e09472105abf61cb8ca40c420631ea726816
celestino.bellone <celestino.bellone@a1e31c00-292e-b01b-3f4f-04888c7956ed>
2011-01-22 20:31:47 +0000

    test modules refactoring
    git-svn-id: https://ejisto.googlecode.com/svn/trunk@48 a1e31c00-292e-b01b-3f4f-04888c7956ed

---
+++ ejisto/test-beans/src/main/java/com/ejisto/hello/beans/HelloWorldBean.java
@@ -0,0 +1,74 @@
+/*
+ * Ejisto, a powerful developer assistant
+ *
+ * Copyright (C) 2011  Celestino Bellone
+ *
+ * Ejisto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ejisto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ejisto.hello.beans;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public class HelloWorldBean implements Serializable {
+
+    private static final long serialVersionUID = 6175502203990776213L;
+    private String title = "Unchanged Title";
+    private String description = "Unchanged Description";
+    private Collection<SimplePropertyValue> propertyValues;
+
+    private int hits = 0;
+    private long timestamp = 0L;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public void setHits(int hits) {
+        this.hits = hits;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Collection<SimplePropertyValue> getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(Collection<SimplePropertyValue> propertyValues) {
+        this.propertyValues = propertyValues;
+    }
+}
---
+++ ejisto/test-webapp/src/main/java/com/ejisto/hello/servlets/HelloServlet.java
@@ -0,0 +1,54 @@
+/*
+ * Ejisto, a powerful developer assistant
+ *
+ * Copyright (C) 2011  Celestino Bellone
+ *
+ * Ejisto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ejisto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ejisto.hello.servlets;
+
+import com.ejisto.hello.beans.HelloWorldBean;
+import com.ejisto.hello.dao.TestDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        HelloWorldBean bean = new HelloWorldBean();
+        TestDao dao = new TestDao();
+        request.setAttribute("bean", bean);
+        request.setAttribute("dao", dao);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+
+}
