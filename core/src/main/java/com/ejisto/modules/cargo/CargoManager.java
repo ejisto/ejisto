6ce077406fb23a7222ce04188d5a5b353ae44261
celestino.bellone <celestino.bellone@gmail.com>
2014-02-14 13:05:15 +0100

    fix gui synchronization

--- core/src/main/java/com/ejisto/modules/cargo/CargoManager.java
+++ core/src/main/java/com/ejisto/modules/cargo/CargoManager.java
@@ -1,7 +1,7 @@
 /*
  * Ejisto, a powerful developer assistant
  *
- * Copyright (C) 2010-2013 Celestino Bellone
+ * Copyright (C) 2010-2014 Celestino Bellone
  *
  * Ejisto is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
