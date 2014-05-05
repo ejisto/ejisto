47260d0dc7323e0af89a7c8a5d26ea3e9e529ee7
celestino.bellone <celestino.bellone@gmail.com>
2014-05-05 08:13:18 +0200

    ejisto-7 second step...

--- application/src/main/webapp/resources/js/controller/application-installer-wizard.js
+++ application/src/main/webapp/resources/js/controller/application-installer-wizard.js
@@ -26,21 +26,17 @@ Ejisto.controllers = Ejisto.controllers || {};
 
         ApplicationInstaller: function ($scope) {
             $scope.descriptor = {};
+            $scope.loading = false;
             $scope.cancel = function () {
                 if (confirm("cancel?")) {
                     $scope.$dismiss('canceled');
                 }
             };
-            $scope.finished = function () {
+            $scope.processCompleted = function () {
                 $scope.$close(true);
             };
         },
         WizardFileSelectionController: function($scope, vertxEventBusService, $upload) {
-
-            $scope.commitSelectFile = function (step) {
-                var descriptor = $scope.descriptor;
-                return FieldService.getFieldsGrouped();
-            };
 
             $scope.onFileSelect = function ($files) {
                 $scope.loading = true;
@@ -50,11 +46,11 @@ Ejisto.controllers = Ejisto.controllers || {};
                        method: 'PUT',
                        file: file
                    }).success(function (data, status, headers, config) {
-                       $scope.sessionID = data.sessionID;
-                       $scope.includedJars = data.resources;
-                       $scope.fileName = file.name;
-                       $scope.loading = false;
-                       $scope.newUploadRequested = false;
+                        $scope.sessionID = data.sessionID;
+                        $scope.descriptor.includedJars = data.resources;
+                        $scope.descriptor.fileName = file.name;
+                        $scope.loading = false;
+                        $scope.newUploadRequested = false;
                    }).error(function () {
                        $scope.loading = false;
                    });
@@ -77,9 +73,102 @@ Ejisto.controllers = Ejisto.controllers || {};
                 }
             });
         },
+        WizardLibFilterController: function($scope) {
+            $scope.commitFilterClasses = function(step) {
+
+            };
+        },
+        StepEvaluator: function($q) {
+            return {
+                commitSelectFile: function (step, scope) {
+                    var deferred = $q.defer();
+                    var descriptor = scope.descriptor;
+                    if(descriptor) {
+                        deferred.resolve(descriptor);
+                    } else {
+                        deferred.reject("descriptor required");
+                    }
+                    return deferred.promise;
+                },
+                commitFilterClasses: function(step, scope) {
+
+                }
+            };
+        },
         install: function(module) {
 
+            module.service('StepEvaluator', this.StepEvaluator);
             module.controller('WizardFileSelectionController', this.WizardFileSelectionController);
+            module.controller('WizardLibFilterController', this.WizardLibFilterController);
+            module.directive("wizardContainer", function(StepEvaluator) {
+                var evaluateCurrentStepData = function(scope, root, steps, currentStep) {
+                    scope.activeStep = currentStep;
+                    scope.showPrevious = angular.isDefined(currentStep.previous);
+                    scope.showNext = angular.isDefined(currentStep.next);
+                    scope.showCancel = true;
+                    scope.showFinish = !angular.isDefined(currentStep.next);
+                    var element = root.find('#'+currentStep.id);
+                    scope.stepTitle=element.attr('data-step-title');
+                    element.addClass('activeStep');
+                };
+                var initSteps = function(steps) {
+                    var previousSteps = _.clone(steps);
+                    previousSteps.unshift(undefined);
+                    var nextSteps = _.clone(steps);
+                    nextSteps.shift();
+                    return _.map(_.zip(steps, previousSteps, nextSteps), function(obj) {
+                        return {
+                            'id' : obj[0],
+                            'previous': obj[1],
+                            'next': obj[2]
+                        }
+                    }).filter(function(o) {
+                        return o.id;
+                    });
+                };
+                return {
+                    restrict: 'A',
+                    link: function(scope, element, attrs) {
+                        var stepIds = _.map(element.find('.wizardStep'), function(el) {
+                            return el.attributes.getNamedItem('id').value;
+                        });
+                        var steps = initSteps(stepIds);
+                        evaluateCurrentStepData(scope, element, steps, _.first(steps));
+                        scope.nextStep = function(currentStep) {
+                            var fnName = "commit" + currentStep.id.charAt(0).toUpperCase() + currentStep.id.slice(1);
+                            if(angular.isFunction(StepEvaluator[fnName])) {
+                                StepEvaluator[fnName](currentStep, scope).then(function(result) {
+                                    var nextStep = _.find(steps, {'id': currentStep.next});
+                                    if(nextStep) {
+                                        element.find('#'+currentStep.id).removeClass('activeStep');
+                                        evaluateCurrentStepData(scope, element, steps, nextStep);
+                                    }
+                                });
+                            }
+                        };
+                        scope.previousStep = function(currentStep) {
+                            var fnName = "clean" + currentStep.id.charAt(0).toUpperCase() + currentStep.id.slice(1);
+                            var activatePreviousStep = function() {
+                                var previousStep = _.find(steps, {'id': currentStep.previous});
+                                if(previousStep) {
+                                    element.find('#'+currentStep.id).removeClass('activeStep');
+                                    evaluateCurrentStepData(scope, element, steps, previousStep);
+                                }
+                            };
+                            if(angular.isFunction(StepEvaluator[fnName])) {
+                                StepEvaluator[fnName](currentStep, scope).then(function(result) {
+                                    activatePreviousStep();
+                                });
+                            } else {
+                                activatePreviousStep();
+                            }
+                        };
+                        scope.cancelProcess = function() {
+                            scope.cancel();
+                        }
+                    }
+                };
+            });
         }
     };
 
